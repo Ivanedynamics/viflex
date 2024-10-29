88cd68cd0a90f2aeae9962e35a22bbf20557ea10
Ivanedynamics <152230484+Ivanedynamics@users.noreply.github.com>
2024-10-28 19:35:34 -0600

    Update connectDB.ts
    fix

--- server/api/connectDB.ts
+++ server/api/connectDB.ts
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 export const connectDatabase = async () => {
-  const URL = "mongodb+srv://ivanedynamics:281mab3oxk@cluster0.mhmsj.mongodb.net/ as string;
+  const URL =
+    "mongodb+srv://ivanedynamics:281mab3oxk@cluster0.mhmsj.mongodb.net/ as string";
 
   try {
     await mongoose.connect(URL, {});
