51074ad07e8f6502dcbad428eb0ca310a3585997
Ivanedynamics <152230484+Ivanedynamics@users.noreply.github.com>
2024-10-31 11:27:24 -0600

    cotizar page with responsive columns products

--- pages/cotizar/index.vue
+++ pages/cotizar/index.vue
@@ -458,7 +458,10 @@ const handleFormSubmit = () => {
           </label>
         </section>
         <section class="mt-4">
-          <ul class="flex flex-col overflow-y-scroll h-full gap-4">
+          <p class="font-bold text-lg py-2">Productos</p>
+          <ul
+            class="flex flex-col h-full gap-4 mobile_s:flex mobile_s:flex-col tablet:grid tablet:grid-cols-2"
+          >
             <li v-for="p in products">
               <CardCartModal
                 :key="p?.id"
@@ -484,8 +487,31 @@ const handleFormSubmit = () => {
             v-if="products?.length !== 0"
             :disabled="products?.length === 0"
             @click="handleFormSubmit"
-            :class="`btn btn-neutral`"
+            :class="`btn btn-primary mobile_s:w-full tablet:w-fit`"
           >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M7.39999 6.32003L15.89 3.49003C19.7 2.22003 21.77 4.30003 20.51 8.11003L17.68 16.6C15.78 22.31 12.66 22.31 10.76 16.6L9.91999 14.08L7.39999 13.24C1.68999 11.34 1.68999 8.23003 7.39999 6.32003Z"
+                stroke="white"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M10.11 13.6501L13.69 10.0601"
+                stroke="white"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+
             Enviar Cotizacion
           </button>
           <NuxtLink v-else to="/productos" class="btn btn-neutral"
